c2555a47b664685c4a75f38535e52b0e00d99e69
WDelenclos <wdelenclos@gmail.com>
2017-05-20 00:22:16 +0200

    Edity

--- start.py
+++ start.py
@@ -1,21 +1,26 @@
-
 #--- Requis
 from sense_hat import SenseHat
 import sys
 import os
 import subprocess
-
+import time
+starttime=time.time()
 
 # --- Demarrage
 sense = SenseHat()
 
-
-
-p = subprocess.Popen(["ps", "-a"], stdout=subprocess.PIPE)
-out, err = p.communicate()
+print "\n\n\n############# Bonjour Wladimir ###########"
 
 while True:
-    if ('Httpd' in out):
-        sense.load_image("wifi.png")
-    else:
-         sense.load_image("close.png")
+  os.system("script.py 1")
+  sense.load_image("wifi.png")
+  time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+# --- Fin
+except KeyboardInterrupt:
+    sense.load_image("close.png")
+    print "Script terminé"
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
