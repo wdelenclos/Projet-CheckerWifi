3faf294bb4fee433b4a5e3d53f029de4ff0cc060
WDelenclos <wdelenclos@gmail.com>
2017-05-20 15:38:27 +0200

    Verification

--- ping.py
+++ ping.py
@@ -4,19 +4,21 @@ sense.set_rotation(180)
 import os
 import time
 
-hostname = "dev.wdelenclos.fr"
+hostname = "dev.wdetttgthhtlenclos.fr"
 starttime=time.time()
 
 while True:
     response = os.system("ping -c 1 " + hostname)
     sense.load_image("hello.png")
-
+    time.sleep(2))
     if response == 0:
-      print hostname, 'is up!'
+      show_message("OK", text_colour=green)
+      print hostname, ' actif !'
       sense.load_image("done.png")
     else:
-      print hostname, 'is down!'
+      show_message("HS", text_colour=red)
+      print hostname, 'INACTIF'
       sense.load_image("erreur.png")
 
-    print "... prochaine verification dans 60s  \n "
+    print "Prochaine verification dans 60s  \n"
     time.sleep(60.0 - ((time.time() - starttime) % 60.0))
