e6b8fcc98c39719a53e213df264d4b01a4554106
WDelenclos <wdelenclos@gmail.com>
2017-05-20 14:46:28 +0200

    Fix

--- start.py
+++ start.py
@@ -6,12 +6,11 @@ import time
 starttime=time.time()
 
 print "\n\n\n############# Script d'actualisation des DNS de dev.wdelenclos.fr ###########"
-i = 0
+
 
 while True:
   os.system('python cf-ddns.py')
-  i += 1
-  print "Verification " + i + " faite"
+  print "... prochaine verification dans 60s  \n "
   time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
 print "\n\n\n############# Script fini ###########"
